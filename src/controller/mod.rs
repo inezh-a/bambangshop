d085b86cd00d5f74b1ac9933ba8c1a7778b36a8b
Rayhan Adhisworo Jardine H <rayhan.jardine@gmail.com>
2024-04-16 20:27:31 +0700

    Create Notification service struct skeleton.

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -1,10 +1,12 @@
 pub mod product;
+pub mod notification;
 
-use rocket::fairing::AdHoc;
+use rocket::{fairing::AdHoc, route};
 
 pub fn route_stage() -> AdHoc {
     return AdHoc::on_ignite("Initializing controller routes...", |rocket| async {
         rocket
             .mount("/product", routes![product::create, product::list, product::read, product::delete])
+            .mount("/notification", route![])
     });
 }
